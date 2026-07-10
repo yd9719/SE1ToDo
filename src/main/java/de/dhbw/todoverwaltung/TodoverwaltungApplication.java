c7d03d958f02cd05b4cbec6b173ff6b0378f1790
Yannic David <yannic.david@aol.com>
2026-07-10 11:36:40 +0200

    grundgerüst mir spring initializr aufgebaut

---
+++ src/main/java/de/dhbw/todoverwaltung/TodoverwaltungApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.todoverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoverwaltungApplication.class, args);
+	}
+
+}
